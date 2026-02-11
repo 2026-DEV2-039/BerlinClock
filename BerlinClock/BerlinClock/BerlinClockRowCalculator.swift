6d0544d77ae952763105efacc6691bf9e308c96c
2026-DEV2-039 <260678539+2026-DEV2-039@users.noreply.github.com>
2026-02-11 18:13:56 +0530

    refactor: BerlinClockRowCalculator refactored the code to check lamp is on and return value code for checking

--- BerlinClock/BerlinClock/BerlinClockRowCalculator.swift
+++ BerlinClock/BerlinClock/BerlinClockRowCalculator.swift
@@ -3,6 +3,11 @@ import Foundation
 struct BerlinClockRowCalculator {
     //MARK: Seconds Row Methods
     func secondsLamp(_ seconds: Int) -> String {
-        return "ON"
+        switch seconds % 2 {
+        case 0:
+            return "ON"
+        default:
+            return "NA"
+        }
     }
 }
