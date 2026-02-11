b002ad6f37ecc26e0b81c7017c1ff3cc1b9e7298
2026-DEV2-039 <260678539+2026-DEV2-039@users.noreply.github.com>
2026-02-11 17:50:04 +0530

    test: Added test case for the all even seconds applicable for Lamp being on

--- BerlinClock/BerlinClockTests/BerlinClockSecondsRowTests.swift
+++ BerlinClock/BerlinClockTests/BerlinClockSecondsRowTests.swift
@@ -3,11 +3,21 @@ import Testing
 
 @Suite("BerlinClockSecondsRow Tests")
 struct BerlinClockSecondsRowTests {
-
+    // Helper Arguments
+    private static let evenSeconds = Array(stride(from: 0, through: 59, by: 2))
+    
     @Test("Test seconds lamp is on when seconds is even")
     func secondsLamp_isOn_whenSecondsIsEven() {
         let calculator = BerlinClockRowCalculator()
         let result = calculator.secondsLamp(0)
+        #expect(result == "ON")
+    }
+    
+    @Test("Seconds lamp is ON for even seconds (parameterized)", arguments: BerlinClockSecondsRowTests.evenSeconds)
+    func secondsLamp_isOn_whenSecondsIsEvenWithArgs(seconds: Int) {
+        let calculator = BerlinClockRowCalculator()
+        let result = calculator.secondsLamp(seconds)
+        
         #expect(result == "ON")
     }
 }
