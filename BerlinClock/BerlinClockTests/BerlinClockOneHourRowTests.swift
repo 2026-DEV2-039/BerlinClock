ff3497ea488cb6716b6f8cb0bac9ae9aabff2e64
2026-DEV2-039 <260678539+2026-DEV2-039@users.noreply.github.com>
2026-02-12 22:58:04 +0530

    refactor: Test cases for utilising helper function to generate expectation states also updated tets case for teh same

--- BerlinClock/BerlinClockTests/BerlinClockOneHourRowTests.swift
+++ BerlinClock/BerlinClockTests/BerlinClockOneHourRowTests.swift
@@ -10,58 +10,53 @@ struct BerlinClockOneHourRowTests {
     func oneHourRow_isAllOff_whenHoursIsZero() {
         let calculator = BerlinClockRowCalculator()
         let result = calculator.oneHourLamps(0)
-        #expect(result.allSatisfy { $0 == .off(.defaultColor) })
+        let expected = expectedOneHoursRows(for: 0)
+        #expect(result == expected)
     }
     
     @Test("Test one hour row one lamp is on when hours is 1")
     func oneHourRow_turnsOnOneLamp_forOneHour() {
         let calculator = BerlinClockRowCalculator()
         let result = calculator.oneHourLamps(1)
-        
-        #expect(result == [
-            .on(.redColor),
-            .off(.defaultColor),
-            .off(.defaultColor),
-            .off(.defaultColor)
-        ])
+        let expected = expectedOneHoursRows(for: 1)
+        #expect(result == expected)
     }
     
     @Test("Test one hour row two lamps are on when hours is 2")
     func oneHourRow_turnsOnTwoLamps_forTwoHours() {
         let calculator = BerlinClockRowCalculator()
         let result = calculator.oneHourLamps(2)
-        
-        #expect(result == [
-            .on(.redColor),
-            .on(.redColor),
-            .off(.defaultColor),
-            .off(.defaultColor)
-        ])
+        let expected = expectedOneHoursRows(for: 2)
+        #expect(result == expected)
     }
     
     @Test("Test one hour row three lamps are on when hours is 3")
     func oneHourRow_turnsOnThreeLamps_forThreeHours() {
         let calculator = BerlinClockRowCalculator()
         let result = calculator.oneHourLamps(3)
-        
-        #expect(result == [
-            .on(.redColor),
-            .on(.redColor),
-            .on(.redColor),
-            .off(.defaultColor)
-        ])
+        let expected = expectedOneHoursRows(for: 3)
+        #expect(result == expected)
     }
     
     @Test("Test one hour row four lamps are on when hours is 4")
     func oneHourRow_turnsOnFourLamps_forFourHours() {
         let calculator = BerlinClockRowCalculator()
         let result = calculator.oneHourLamps(4)
-        
-        #expect(result == [
-            .on(.redColor),
-            .on(.redColor),
-            .on(.redColor),
-            .on(.redColor)
-        ])
+        let expected = expectedOneHoursRows(for: 4)
+        #expect(result == expected)
+    }
+}
+
+// MARK: Helper function for test case
+extension BerlinClockOneHourRowTests {
+    func expectedOneHoursRows(for hours: Int) -> [BerlinClockLampsState] {
+        let onCount = hours % 5
+        return (1...4).map { index in
+            if index <= onCount {
+                return .on(.redColor)
+            } else {
+                return .off(.defaultColor)
+            }
+        }
     }
 }
