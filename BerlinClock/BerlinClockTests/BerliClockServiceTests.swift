8f5004dd7f91c721012a2967329120daf80ce148
2026-DEV2-039 <260678539+2026-DEV2-039@users.noreply.github.com>
2026-02-15 16:46:28 +0530

    test: Added test case for the timer emits values

--- BerlinClock/BerlinClockTests/BerliClockServiceTests.swift
+++ BerlinClock/BerlinClockTests/BerliClockServiceTests.swift
@@ -7,7 +7,7 @@ import Combine
 @Suite("BerlinClockService Tests")
 struct BerlinClockServiceTests {
     
-    @Test("Emits initial value immediately")
+    @Test("Test emits initial value immediately")
     func initialEmission() {
         let fixedDate = Date(timeIntervalSince1970: 0)
         let mockTime = MockSystemTimeProvider(date: fixedDate)
@@ -27,4 +27,41 @@ struct BerlinClockServiceTests {
         _ = cancellable
     }
     
+    @Test("Test emits when timer sends value")
+    func emitsOnTimerSend() {
+        
+        let calendar = Calendar(identifier: .gregorian)
+        
+        let date = calendar.date(from: DateComponents(
+            year: 2024,
+            month: 1,
+            day: 1,
+            hour: 12,
+            minute: 34,
+            second: 56
+        ))!
+        
+        let mockTime = MockSystemTimeProvider(date: date)
+        let mockTimer = MockSystemTimePublisher()
+        
+        let service = BerlinClockService(
+            calendar: calendar,
+            timeProvider: mockTime,
+            timerPublisher: mockTimer
+        )
+        
+        var values: [DigitalTime] = []
+        
+        let cancellable = service.timePublisher
+            .sink { values.append($0) }
+        
+        mockTimer.send(date)
+        
+        #expect(values.count == 2)
+        #expect(values[1].hours == 12)
+        #expect(values[1].minutes == 34)
+        #expect(values[1].seconds == 56)
+        
+        _ = cancellable
+    }
 }
