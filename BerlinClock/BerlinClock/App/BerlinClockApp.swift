6c40bdfbe12c099d7b92ad84243f5ee77b7c91a6
2026-DEV2-039 <260678539+2026-DEV2-039@users.noreply.github.com>
2026-02-16 12:41:00 +0530

    add: Added code for the BerlinClockView to App and assembled all dependencies

--- BerlinClock/BerlinClock/App/BerlinClockApp.swift
+++ BerlinClock/BerlinClock/App/BerlinClockApp.swift
@@ -4,6 +4,25 @@ import SwiftUI
 struct BerlinClockApp: App {
     var body: some Scene {
         WindowGroup {
+            BerlinClockView(
+                viewModel: makeBerlinClockViewModel()
+            )
         }
+    }
+}
+
+private extension BerlinClockApp {
+    // MARK: - Dependency assembly
+    private func makeBerlinClockViewModel() -> BerlinClockViewModel {
+        let timeProvider = SystemTimeProvider()
+        let timePublisher = SystemTimerPublisher()
+        let clockService = BerlinClockService(calendar: .current,
+                                              timeProvider: timeProvider,
+                                              timerPublisher: timePublisher)
+        
+        let berlincRowStateConvertor = BerlinClockRowCalculator()
+    
+        return BerlinClockViewModel(clockService: clockService,
+                                    berlinLampStateConvertor: berlincRowStateConvertor)
     }
 }
